7b0efff5b80c0718cc79153ea6ed3c9997ec347a
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-01-22 11:09:55 +0000

    Initial setup of master

---
+++ src/main/scala/app/GreeterApplication.scala
@@ -0,0 +1,13 @@
+package app
+
+import scala.io.StdIn
+
+object GreeterApplication extends App {
+
+  def greet(name : String) : Unit = println(s"Hello $name")
+
+  val name = () => StdIn.readLine("What is your name? ")
+
+  greet(name())
+
+}
